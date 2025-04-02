1c572c01488876b1dde4ba17e3f827e207f62113
n00b-cat <157589523+n00b-cat@users.noreply.github.com>
2025-04-02 11:57:16 +0200

    clicking cards

--- js/deck.js
+++ js/deck.js
@@ -2,10 +2,44 @@
 const suits = ["heart", "spade", "clower", "diamond"]
 let deck = []
 
+class Card {
+    constructor(suit, number,x, y) {
+        this.suit = suit;
+        this.number = number;
+        this.x = x;
+        this.y = y;
+        this.width = 55;
+        this.height = 76;
+        this.image = new Image();
+        this.image.src = "sprites/"+this.suit+".png";
+        this.image.onload = () => {}
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.image, (this.number - 1) * 55 + (this.number - 1), 0, this.width, this.height, this.x, this.y, this.width, this.height);
+    }
+
+    clickcard(xmouse, ymouse) {
+        if (xmouse > this.x && xmouse < this.x + this.width && ymouse > this.y && ymouse < this.y + this.height) {
+            console.log("played: " + this.suit + " " + this.number);
+        }
+    }
+}
+
 for (let type = 0; type < 4; type++) {
-    let cardtype = suits[type]
+    let suit = suits[type]
 
     for (let i = 1; i <= 13; i++) {
-        deck.push({ suit: cardtype, number: i })
+        deck.push(new Card(suit, i))
+    }
+}
+
+// Card shuffle
+function card_shuffle() {
+    for (let i = 0; i < deck.length; i++) {
+        let random = Math.floor(Math.random() * deck.length)
+        let temp = deck[i]
+        deck[i] = deck[random]
+        deck[random] = temp
     }
 }
--- js/game.js
+++ js/game.js
@@ -5,54 +5,54 @@ let players = [
     { name: "Player2", hand: [] },
     { name: "Player3", hand: [] },
     { name: "Player4", hand: [] },
-    { name: "Player5", hand: [] },
 ]
 
 // canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// Card shuffle
-function card_shuffle() {
-    for (let i = 0; i < deck.length; i++) {
-        let random = Math.floor(Math.random() * deck.length)
-        let temp = deck[i]
-        deck[i] = deck[random]
-        deck[random] = temp
-    }
-}
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 // Card distribution
 function deal_cards() {
     players.forEach(player => {
         player.hand = []
     })
-    
+
     for (let i = 0; i < deck.length; i++) {
         players[i % players.length].hand.push(deck[i])
     }
 }
 
-// card size 55x76 1px gap
+canvas.addEventListener("click", (event) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+
+    for (let player of players) {
+        for (let card of player.hand) {
+            card.clickcard(mouseX, mouseY);
+        }
+    }
+});
+
 
 // updating game
 function update() {
 
 }
+
 // visualsing game fr
 function draw() {
 
-    // clearing the screen from last frame
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Card drawing
     for (let y = 0; y < players.length; y++) {
         for (let x = 0; x < players[y].hand.length; x++) {
             let card = players[y].hand[x]
-
-            let image = new Image();
-            image.src = "sprites/" + card.suit + ".png";
-            ctx.drawImage(image, (card.number - 1) * 55 + (card.number - 1), 0, 55, 76, x * 60, y * 80, 55, 76);
+            card.x = 10 + x * 60;
+            card.y = 10 + y * 100;
+            card.draw(ctx);
         }
     }
 }
